be934d2a26f041d38c9c6a118d6709c694354131
morsy065 <morsy065@mymail.unisa.edu>
2025-11-24 22:56:03 +1030

    mammal class and subclasses (Lion, Giraffe, Elephant)

--- animal.py
+++ animal.py
@@ -8,36 +8,81 @@ This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
 class Animal:
-    def __init__(self, name, age, species, diet, sex):
-        self.__name = name
-        self.__age = age
-        self.__species = species
-        self.__diet = diet
-        self.__sex = sex
+    def __init__(self, name, species, age, diet, sex):
+        self._name = name
+        self._species = species
+        self._age = age
+        self._diet = diet
+        self._sex = sex
 
     def get_name(self):
-        return self.__name
+        return self._name
     
     def get_age(self):
-        return self.__age
+        return self._age
     
     def get_species(self):
-        return self.__species
+        return self._species
     
     def get_diet(self):
-        return self.__diet
+        return self._diet
     
     def get_sex(self):
-        return self.__sex
-    
-    def method(self):
-        s
+        return self._sex
 
 class Mammal(Animal):
-    def __init__(self, name, age, species, dietary_needs, sex):
-        super().__init__(name, age, species, dietary_needs, sex)
+    def __init__(self, name, species, age, diet, sex):
+        super().__init__(name, species, age, diet, sex)
 
 
 class Lion(Mammal):
     def __init__(self, name, age):
-        super().__init__(name, age, "Lion", "Carnivore", "Male")
+        super().__init__(name, "Lion", age, "Carnivore", "Male")
+
+    def roar(self):
+        return f"{self._name} the {self._species} makes a loud ROARR!"
+    
+    def pouncing(self):
+        return f"{self._name} the {self._species} pounces around."
+    
+    def hunt(self):
+        return f"{self._name} the {self._species} stalks the visitors."
+    
+    def desc(self):
+        return f" Name: {self._name} \n Species: {self._species} \n Age: {self._age} \n Dietary Needs: {self._diet} \n Sex: {self._sex}"
+    
+class Giraffe(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, "Giraffe", age, "Herbivore", "Female")
+
+    def eats(self):
+        return f"{self._name} the {self._species} reaches for the tree."
+    
+    def sleeps(self):
+        return f"{self._name} the {self._species} sits down to sleep."
+    
+    def feeds(self):
+        return f"{self._name} the {self._species} feeds her kid."
+    
+    def desc(self):
+        return f" Name: {self._name} \n Species: {self._species} \n Age: {self._age} \n Dietary Needs: {self._diet} \n Sex: {self._sex}"
+    
+class Elephant(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, "Elephant", age, "Herbivore", "Male")
+
+    def trumpet(self):
+        return f"{self._name} the {self._species} blows his trunk"
+    
+    def spray(self):
+        return f"{self._name} the {self._species} sprays water around."
+    
+    def reach(self):
+        return f"{self._name} the {self._species} reaches for thr visitors with its trunk."
+    
+    def desc(self):
+        return f" Name: {self._name} \n Species: {self._species} \n Age: {self._age} \n Dietary Needs: {self._diet} \n Sex: {self._sex}"
+    
+
+Leo = Lion("Leo", 6)
+print(Leo.desc())
